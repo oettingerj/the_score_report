74ee6d172525b876d19f3361ba904ae65b877bfb
Josh Oettinger <j.oettinger@fetchrewards.com>
2022-03-24 13:21:16 -0500

    Yeeted windows and android

--- android/app/src/main/kotlin/com/example/the_score_report/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.the_score_report
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:the_score_report/components/card.dart';
 
 void main() {
   runApp(const MyApp());
